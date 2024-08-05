dc0e897a49ee6bb67e6a60b9b6ce17ea4bd33093
akuten1298 <codingaku@gmail.com>
2024-08-04 17:43:58 -0700

    Deleted employee and bank codebase

--- mockito.example/src/main/java/com/example/TransactionCode.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-
-import java.util.function.Function;
-
-@FunctionalInterface
-public interface TransactionCode<T> extends Function<EmployeeRepository, T> {
-
-}
--- mockito.example/src/main/java/com/example/TransactionManager.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-public interface TransactionManager {
-
-	<T> T doInTransaction(TransactionCode<T> code);
-
-}
